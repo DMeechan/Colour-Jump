6acdea838f5459e082beb0276bbeee937bda5764
DMeechan <daniel.meechan.1+git@gmail.com>
2017-09-13 11:51:29 +0100

    Added Fabric and Crashlytics

---
+++ Colour Jumper/Colour Jumper/Colour Jump/AppDelegate.swift
@@ -0,0 +1,38 @@
+//
+//  AppDelegate.swift
+//  Colour Trek
+//
+//  Created by Daniel Meechan on 31/08/2017.
+//  Copyright © 2017 Rogue Studios. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import GoogleMobileAds
+import Fabric
+import Crashlytics
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    
+    window = UIWindow(frame: UIScreen.main.bounds)
+    window?.makeKeyAndVisible()
+    
+    window?.rootViewController = GameViewController()
+    
+    let appID = "ca-app-pub-4605466962808569~5941669316"
+    GADMobileAds.configure(withApplicationID: appID)
+    FirebaseApp.configure()
+    Fabric.with([Crashlytics.self])
+    
+    return true
+    
+  }
+
+
+}
+
